8e26504705385bbcc23aee98c85825807bf5359c
ASHWIN-PRADEEP25 <69059265+ASHWIN-PRADEEP25@users.noreply.github.com>
2021-07-01 01:01:34 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -2,21 +2,6 @@
 #include <assert.h>
 #include <stdlib.h>
 
-// void swap_max(int [], int, int );
-void ssort(int [], int);
-
-int main(void)
-{
-    int array[]={56,61,86,26,19,56,75,95,1,2,35,22};
-    printf("\n");
-    for(int i=0;i<12;i++)
-    {
-      printf("%d ",array[i]);  
-    }
-    printf("\n\n");
-   ssort(array,12);
-   printf("\n\n");
-}
 
 void swap_max(int a[], int L, int n)
 {
