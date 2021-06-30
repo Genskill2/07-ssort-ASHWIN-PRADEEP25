e49714b6e6dcbaef4e4c47717036a76fbdff4e38
ASHWIN-PRADEEP25 <69059265+ASHWIN-PRADEEP25@users.noreply.github.com>
2021-07-01 00:58:15 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -2,11 +2,27 @@
 #include <assert.h>
 #include <stdlib.h>
 
-void swap_max(int a[], int l, int n)
+// void swap_max(int [], int, int );
+void ssort(int [], int);
+
+int main(void)
+{
+    int array[]={56,61,86,26,19,56,75,95,1,2,35,22};
+    printf("\n");
+    for(int i=0;i<12;i++)
+    {
+      printf("%d ",array[i]);  
+    }
+    printf("\n\n");
+   ssort(array,12);
+   printf("\n\n");
+}
+
+void swap_max(int a[], int L, int n)
 {
   int swp=a[n-1];
   int count;
-  for(int i=n-1 ; i<l ; i++)
+  for(int i=n-1 ; i<L ; i++)
   {
     if(a[i]>swp)
     {
@@ -14,26 +30,20 @@ void swap_max(int a[], int l, int n)
       count=i;
     } 
   }
-  printf("%d\n\n",swp);
   a[count]=a[n-1];
   a[n-1]=swp;
+} 
 
-  for(int i=0;i<l;i++)
-  {
-      printf("%d ",a[i]);  
-  }
-}
 
-int main(void)
+void ssort(int arr[], int l)
 {
-    int arr[]={56,61,86,26,19,56,75,95,1,2,35,22};
-    printf("\n");
-    for(int i=0;i<12;i++)
-    {
-      printf("%d ",arr[i]);  
-    }
-    printf("\n\n");
-   swap_max(arr,12,4); 
-   printf("\n\n");
-   
+  for(int p=0;p>l-1;p++)  
+  {
+    swap_max(arr,l,p);
+  }
+
+  for(int p=0;p>l-1;p++)
+  {
+      printf("%d ",arr[p]); 
+  }
 }
